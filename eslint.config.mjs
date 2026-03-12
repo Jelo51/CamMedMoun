3d144d23136604e02bc0a7bad9f7cb00bce351ea
amnmdk <objectifesgi@gmail.com>
2026-03-12 12:58:34 +0100

    eslint de merde

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "react/prop-types": "off",
+      "react/no-unescaped-entities": "off"
+    }
+  }
+];
